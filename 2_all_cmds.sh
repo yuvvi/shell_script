9d4d2111449b3e5d5cdb0430376322e8cd88430b
yuvaram <yuvaram.aligeti@gmail.com>
2018-12-05 14:39:52 +0530

    Update 2_all_cmds.sh

--- 2_all_cmds.sh
+++ 2_all_cmds.sh
@@ -1,1 +1,5 @@
 cat 0_csv.txt | awk -F " " '{print $9}' | sed -e "s/[0-9]//g" | sort -u > csv_different_file.txt
+
+cat csv_extension.csv | awk 'NF>1{print $NF}' | sed -e "s/[0-9]//g" | sort -u > csv_naming_conventions.txt
+
+
