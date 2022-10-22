de5335ca80b86b54360bdde3f3a142f4ab1cfc5e
yuvaram <yuvaram.aligeti@gmail.com>
2022-10-22 12:28:07 +0530

    Update bashTips.sh

--- bashTips.sh
+++ bashTips.sh
@@ -13,5 +13,12 @@ if [ ls==TRUE ]; then echo "success"; else echo "fail"; fi
 
 echo "[ "$ls"==TRUE ]"
 if [ "$ls"==TRUE ]; then echo "success"; else echo "fail"; fi
-
+#--------------COMMENTS--------------
 # Above all comparions returns "success".
+#Different command execution success or fail validation in "if" condition.
+
+#Bourne shell (sh) program began its life in the 70s with UNIX OS.
+#Bourne-Again shell (bash) - 90s
+
+#= , == within [] is considered as comparison only, not assignment operator.
+#------------------------------------
