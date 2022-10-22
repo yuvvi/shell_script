6b3c5cb323a1d1ce2f790bf72b68e8fba9457a19
yuvaram <yuvaram.aligeti@gmail.com>
2022-10-22 12:44:37 +0530

    Update bashTips.sh

--- bashTips.sh
+++ bashTips.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# If you try to use an undefined variable, bash will not treat it as an error.
+# Bash will substitute an empty string and proceed.
+# To fix above scenario, "set -eu" can be used. Error will reported at the end of execution.
+set -eu
+
 echo "[ ls == 0 ]"
 if [ ls == 0 ]; then echo "success"; else echo "fail"; fi
 
